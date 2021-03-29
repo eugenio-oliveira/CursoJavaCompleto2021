24ca3df1f244d459a09e6649f86d0ef826836c4b
Eugênio Oliveira <eugenio.p.oliveira@outlook.com>
2021-03-29 13:06:54 -0300

    exercício 02 e classe de operadores

---
+++ src/Exercicio02.java
@@ -0,0 +1,22 @@
+
+public class Exercicio02 {
+
+	public static void main(String[] args) {
+		
+		String nome = "Anthony Stark";
+		int idade = 39;
+		String sexo = "Masculino";
+		String estadoCivil = "Casado";
+		double salario = 20000;
+		
+		System.out.println(
+				"O(A) trabalhador(a): " + nome + 
+				" de " + idade + 
+				" do sexo " + sexo + 
+				" com estado civil de " + estadoCivil + 
+				" com salário de R$" + salario + 
+				" está devidamente empregado(a) nessa empresa."
+				);
+	}
+
+}
---
+++ src/Operadores.java
@@ -0,0 +1,21 @@
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		
+		int numero1 = 10;
+		int numero2 = 12;
+		int soma = numero1 + numero2;
+		
+		System.out.println("A soma dos números " + numero1 + " e " + numero2 + " é: " + soma);
+		
+		int minuendo = 27;
+		int subtraendo = 12;
+		int diferenca = numero1 - numero2;
+		
+		System.out.println("A diferenda da subtração dos números " + minuendo + " e " + subtraendo + " é: " + diferenca);
+		
+
+	}
+
+}
